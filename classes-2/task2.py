252ec4a3bea104946e4b52299155e566ca109b84
Dave <daizcoding@gmail.com>
2019-10-12 15:23:40 +0300

    added classes-2 hw

---
+++ classes-2/task2.py
@@ -0,0 +1,7 @@
+# Task 2 extends the first one. Now you should add 2-3 types of books (for
+# example, Schoolbook, Magazine etc)
+
+# Library class should still have only one list for all books in contains
+# but different methods for grouping by types. For example:
+#   - fetch_schoolbooks
+#   - fetch_magazines
