cb0d3d89b9cd59ee56eed432d43d1df9c9823592
Dave <daizcoding@gmail.com>
2019-10-16 10:38:40 +0300

    added first-class and classes-3

---
+++ classes-3/task1.py
@@ -0,0 +1,58 @@
+# Implement a simple composition of 4 classes: ATM, Client, Card, Bank
+#
+# Bank(name) + accounts = [] - list of all accounts opened in the current bank
+# methods:
+#   open_account(client: Client) - takes an instance of Client class as an
+# argument and creates a new instance of Card bound to this client (and returns
+# it). ! A client can only open one account per bank !
+#   close_account(card: Card) - removes specified instance of Card
+#
+# ATM(bank: Bank, amount: float) - takes an instance of Bank class as an
+# argument (the bank this card is bound to) and the amount of money in
+# the current ATM.
+# methods:
+#   withdraw(card: Card, sum: float) - takes two arguments: an instance of
+# Card and sum which is needed to be withdrawn. You should consider 2 cases:
+#       1) when the amount of money in the bank is less than the sum
+#       2) when the ATM and Card are bound to different banks (it shouldn't
+# let you withdraw)
+#   add(card: Card, sum: float) - add the specified amount of money to the card
+#   change_pin(card: Card, old_pin: int, new_pin: int) - change card's pin
+#
+# Card(account: int, balance: float, pin: int, owner: Client, bank: Bank) -
+# account is just a random 5-digit number. Pin is 0000 by default
+# methods:
+#   transfer_money(card, amount) - transfers the money from the current card to
+# the specified one
+#
+# Client(name: str) + cards = [] - list of cards bound to the client
+# methods:
+#   show_total_balance - returns the sum of money from all cards owned by the
+# client
+
+
+class ATM:
+    pass
+
+
+class Bank:
+    pass
+
+
+class Client:
+    pass
+
+
+class Card:
+    pass
+
+
+client = Client("John")
+bank = Bank("PrivatBank")
+atm = ATM(bank, 10000)
+
+card = bank.open_account(client)
+assert card.balance == 0.0
+
+atm.add(card, 500)
+assert card.balance == 500.0
